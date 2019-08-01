2f9fa72fd542dac6356f401b5854d24fddfaf413
ALLANKAKAI <allankakai26@gmail.com>
2019-08-01 20:48:54 +0300

    modified files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import './index.css';
+
+import{getUsers} from './api/userApi';
+
 import numeral from 'numeral';
 
-const courseValue = numeral(1000).format(0.00);
-console.log(`i would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
+// Populate tabe of users with API call
+getUsers().then(result => {
+	let usersBody = "";
+
+	result.forEach(user => {
+		usersBody+=`<tr>
+			<td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+			<td<${user.id}</td>
+			<td>${user.firstName}</td>
+			<td>${user.lastName}</td>
+			<td>${user.email}</td>
+			</tr>`		
+	});
+
+	global.document.getElementById('users').innerHTML = usersBody;
+});
